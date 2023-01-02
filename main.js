171508e9020341b0842fefa3dd99032b3677fd31
Damian Galeano <dj.damian.b@hotmail.com>
2023-01-03 00:00:10 +0100

    message

---
+++ main.js
@@ -0,0 +1,10 @@
+// ReferenceError : identificador is not defined
+ 
+function greetting() {
+    var message = "this code is inside a function";
+
+    console.error(message);
+};
+
+console.error(greetings);
+
